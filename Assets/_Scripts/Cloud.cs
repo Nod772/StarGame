75db32504b743b4ec17943c3c3073d3403222aac
Андрій Подніжний <andriy.podnizhniy@gmail.com>
2021-05-12 18:33:56 +0300

    first

---
+++ Assets/_Scripts/Cloud.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+    [Header("Set in Inteseptor")]
+    public GameObject cloudSphere;
+    public int numShepesMin = 6;
+    public int numShepesMax = 10;
+    
+    public Vector3 sphereOffsetScale = new Vector3(5, 2, 1);
+    public Vector2 sphereScaleX = new Vector2(4, 8);
+    public Vector2 sphereScaleY = new Vector2(3, 4);
+    public Vector2 sphereScaleZ = new Vector2(2, 4);
+    public float scaleYMin = 2f;
+
+    private List<GameObject> spheres;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spheres = new List<GameObject>();
+        int num = Random.Range(numShepesMin, numShepesMax);
+        for (int i = 0; i < num; i++)
+        {
+            GameObject sp = Instantiate<GameObject>(cloudSphere);
+            spheres.Add(sp);
+            Transform spTrans = sp.transform;
+            spTrans.SetParent(this.transform);
+
+            Vector3 offset = Random.insideUnitSphere;
+
+            offset.x *= sphereOffsetScale.x;
+            offset.y *= sphereOffsetScale.y;
+            offset.z *= sphereOffsetScale.z;
+            spTrans.localPosition = offset;
+
+            Vector3 scale = Vector3.one;
+            scale.x = Random.Range(sphereScaleX.x, sphereScaleX.y);
+            scale.y = Random.Range(sphereScaleY.x, sphereScaleY.y);
+            scale.z = Random.Range(sphereScaleZ.x, sphereScaleZ.y);
+
+            scale.y *= 1 - (Mathf.Abs(offset.x) / sphereOffsetScale.x);
+            scale.y = Mathf.Max(scale.y, scaleYMin);
+
+            spTrans.localScale = scale;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    Restart();
+        //}
+        
+    }
+    private void Restart()
+    {
+        foreach (GameObject sp in spheres)
+        {
+            Destroy(sp);
+        }
+        Start();
+    }
+}
---
+++ Assets/_Scripts/Slingshot.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    static private Slingshot S;
+
+    [Header("Set in Inteseptor")]
+    public GameObject prefabProjectile;
+
+    [Header("Set in Dynamically")]
+    public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimimgMode;
+    public float velocityMult = 8f;
+    private Rigidbody projectileRigibody;
+
+    // Start is called before the first frame update
+
+
+    static public Vector3 LAUNCH_POS
+    {
+        get
+        {
+            if (S==null)
+            {
+                return Vector3.zero;
+            }
+            else
+            {
+                return S.launchPos;
+            }
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if (!aimimgMode)
+        {
+            return;
+        }
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        Vector3 mouseDelta = mousePos3D - launchPos;
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if (mouseDelta.magnitude>maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
+        if (Input.GetMouseButton(0))
+        {
+            aimimgMode = false;
+            projectileRigibody.isKinematic = false;
+            projectileRigibody.velocity = -mouseDelta * velocityMult;
+            FollowCam.POI = projectile;
+            projectile = null;
+            MissionDemolition.ShotFired();
+            ProjectileLine.S.poi = projectile;
+        }
+       
+    }
+
+    private void Awake()
+    {
+        S = this;
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
+    }
+
+    private void OnMouseEnter()
+    {
+        print("Slignshot:OnMouseEnter()");
+        launchPoint.SetActive(true);
+    }
+    private void OnMouseExit()
+    {
+        print("Slignshot:OnMouseExit()");
+
+        launchPoint.SetActive(false);
+    }
+    private void OnMouseDown()
+    {
+        aimimgMode = true;
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        projectile.transform.position = launchPos;
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
+        projectileRigibody = projectile.GetComponent<Rigidbody>();
+        projectileRigibody.isKinematic = true;
+    }
+}
